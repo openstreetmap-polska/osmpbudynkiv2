09c3e725fce81084a6be4333fcf03ceb9404d181
Tomasz Taraś <tomasztaras@outlook.com>
2026-08-22 13:19:59 +0200

    add script for creating fresh db

---
+++ scripts/run_release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set -o allexport && source .env && set +o allexport
+
+cargo build --release
+
+./target/release/osmpbudynkiv2 --config example_config.toml import full
+./target/release/osmpbudynkiv2 --config example_config.toml compare full
+
+./target/release/osmpbudynkiv2 --config example_config.toml update osm
+./target/release/osmpbudynkiv2 --config example_config.toml queue drain
