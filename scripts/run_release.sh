0c5b5d9d181f5dc3789f7a4481e030da0bc69405
Tomasz Taraś <tomasztaras@outlook.com>
2026-08-28 17:59:15 +0200

    use init instead of specific subcommands

--- scripts/run_release.sh
+++ scripts/run_release.sh
@@ -6,8 +6,10 @@ set -o allexport && source .env && set +o allexport
 
 cargo build --release
 
-./target/release/osmpbudynkiv2 --config example_config.toml import full
-./target/release/osmpbudynkiv2 --config example_config.toml compare full
+# ./target/release/osmpbudynkiv2 --config example_config.toml import full
+# ./target/release/osmpbudynkiv2 --config example_config.toml compare full
 
-./target/release/osmpbudynkiv2 --config example_config.toml update osm
-./target/release/osmpbudynkiv2 --config example_config.toml queue drain
+# ./target/release/osmpbudynkiv2 --config example_config.toml update osm
+# ./target/release/osmpbudynkiv2 --config example_config.toml queue drain
+
+./target/release/osmpbudynkiv2 --config example_config.toml init
